b4ead4f3f1b66a369fe9573b37b0cb56e6810051
Jens Brauer <jensb89@users.noreply.github.com>
2018-11-07 19:39:38 +0100

    Initial Commit

---
+++ CutListCreatorX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CutListCreatorX
+//
+//  Created by Jens Brauer on 07.11.18.
+//  Copyright © 2018 Jens Brauer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
